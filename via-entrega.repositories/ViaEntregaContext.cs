2aec1ac48d57cc8c8b2b94086f8fff55e8229b67
Matheus-henryke <matheus.henryke16@gmail.com>
2025-08-07 21:01:40 -0300

    ajustes

--- via-entrega.repositories/ViaEntregaContext.cs
+++ via-entrega.repositories/ViaEntregaContext.cs
@@ -1,20 +1,39 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using via_entrega.entities;
+using via_entrega.entities.Orders;
+using via_entrega.entities.Registrations;
+using via_entrega.repositoriess.Configuration;
 
 namespace via_entrega.repositoriess
 {
-	public class ViaEntregaContext : DbContext
-	{
-		public ViaEntregaContext(DbContextOptions<ViaEntregaContext> configuration) : base(configuration)
-		{
-		}
+    public class ViaEntregaContext : DbContext
+    {
+        public ViaEntregaContext(DbContextOptions<ViaEntregaContext> configuration) : base(configuration)
+        {
+        }
+        public DbSet<Pessoa> Pessoas { get; set; }
+        public DbSet<PessoaFisica> PessoasFisicas { get; set; }
+        public DbSet<PessoaJuridica> PessoasJuridicas { get; set; }
+        public DbSet<Veiculo> Veiculos { get; set; }
+        public DbSet<CollectionOrder> CollectionOrders { get; set; }
+        public DbSet<DeliveryOrder> DeliveryOrders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<DadosContato> DadosContatos { get; set; }
+        public DbSet<DadosEndereco> DadosEnderecos { get; set; }
 
-		public DbSet<Driver> Drivers { get; set; }
-		public DbSet<Veiculo> Veiculos { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-		{
-			base.OnModelCreating(modelBuilder);
-		}
-	}
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new DadosContatoConfiguration());
+            modelBuilder.ApplyConfiguration(new PessoaConfiguration());
+            modelBuilder.ApplyConfiguration(new PessoaFisicaConfiguration());
+            modelBuilder.ApplyConfiguration(new PessoaJuridicaConfiguration());
+            modelBuilder.ApplyConfiguration(new VeiculoConfiguration());
+            modelBuilder.ApplyConfiguration(new CollectionOrderConfiguration());
+            modelBuilder.ApplyConfiguration(new DeliveryOrderConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new DadosEnderecoConfiguration());
+
+        }
+    }
 }
