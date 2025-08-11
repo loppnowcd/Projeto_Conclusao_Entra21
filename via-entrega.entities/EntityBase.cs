5df8201ec82795fe002767b90838e9955eef29a7
Christian Loppnow <loppnow.cd@gmail.com>
2025-08-11 20:33:42 -0300

    Criado EntityBase.cs

---
+++ via-entrega.entities/EntityBase.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace via_entrega.entities
+{
+	public abstract class EntityBase : IEntityBase
+	{
+		public Guid Id { get; set; }
+		public bool Active { get; set; }
+		public DateTime CreatedAt { get; set; }
+		public DateTime UpdatedAt { get; set; }
+	}
+}
