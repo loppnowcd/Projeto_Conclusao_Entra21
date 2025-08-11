aa0abc4731cf5592cba26f341509974fc7584ca5
Christian Loppnow <loppnow.cd@gmail.com>
2025-08-11 20:35:51 -0300

    Criado DadosDocumentos.cs

---
+++ via-entrega.entities/registrations/DadosDocumentos.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace via_entrega.entities.Registrations
+{
+    public class DadosDocumentos
+    {
+               		
+		public string CNH { get; set; }
+
+        [Display(Name = "Data de Expedição da CNH")]
+        public DateTime CNHExpedido { get; set; }
+    }
+}
