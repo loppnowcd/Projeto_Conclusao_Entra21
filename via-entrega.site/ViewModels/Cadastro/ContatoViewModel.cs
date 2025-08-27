52af0b9c30d3efcbdb7127e4966a64e87dc1ff05
luiz henrique <henriquelondero@hotmail.com>
2025-08-27 20:13:45 -0300

    ajustestelae ca
    Implementa gerenciamento de contatos com novo layout
    
    Adiciona o controlador `ContatosController` para gerenciar operações de contatos, incluindo listagem, criação, edição e exclusão. Atualiza `CadastroViewModel` para incluir a propriedade obrigatória `TipoPessoa`. Modifica `ContatoViewModel` para incluir um identificador (`Id`) e métodos de mapeamento.
    
    Reformula a página `Index.cshtml` com suporte a Bootstrap 5, apresentando uma interface moderna para gerenciar contatos. Adiciona o modal `_FormModal.cshtml` para criação e edição de contatos, com validação de campos obrigatórios. Implementa a tabela `_Table.cshtml` para exibir contatos de forma interativa.
    
    Atualiza o layout `_Layout.cshtml` para uma nova estrutura de navegação e estilo, e adiciona o arquivo `site-viaentrega.css` para definir estilos globais e específicos da aplicação.

--- via-entrega.site/ViewModels/Cadastro/ContatoViewModel.cs
+++ via-entrega.site/ViewModels/Cadastro/ContatoViewModel.cs
@@ -1,9 +1,36 @@
-﻿namespace ViewModels.Cadastro
+﻿using System.Runtime.CompilerServices;
+using via_entrega.entities.Registrations;
+
+namespace ViewModels.Cadastro
 {
-    public class ContatoViewModel
-    {
-        public string Celular { get; set; }
-        public string? Telefone { get; set; }
-        public string Email { get; set; }
-    }
+	public class ContatoViewModel
+	{
+		public Guid Id { get; set; }
+		public string Celular { get; set; }
+		public string? Telefone { get; set; }
+		public string Email { get; set; }
+
+
+		public static ContatoViewModel MapearParaViewModel(DadosContato dadosContato)
+		{
+			return new ContatoViewModel
+			{
+				Id = dadosContato.Id,
+				Celular = dadosContato.Celular,
+				Telefone = dadosContato.Telefone,
+				Email = dadosContato.Email
+			};
+		}
+
+		public DadosContato MapearParaEntidade()
+		{
+			return new DadosContato
+			{
+				Id = this.Id,
+				Celular = this.Celular,
+				Telefone = this.Telefone,
+				Email = this.Email
+			};
+		}
+	}
 }
